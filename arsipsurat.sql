b4b5118cfd0e8a5e262ad5f6494b2b82b5172709
= <=>
2021-10-25 21:18:21 +0700

    database

---
+++ arsipsurat.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2021 at 04:17 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arsipsurat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(2) NOT NULL,
+  `nama_admin` varchar(50) NOT NULL,
+  `username_admin` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id_admin`, `nama_admin`, `username_admin`, `password`, `gambar`) VALUES
+(1, 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin.jpg'),
+(1931710009, 'Uswatun Khasanah', 'uswa', '93a116d086cb5586f0cd2d5e03b51f1fa61fd861', 'uswa.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_bagian`
+--
+
+CREATE TABLE `tb_bagian` (
+  `id_bagian` int(11) NOT NULL,
+  `nama_bagian` varchar(120) NOT NULL,
+  `username_admin_bagian` varchar(50) NOT NULL,
+  `password_bagian` varchar(50) NOT NULL,
+  `nama_lengkap` varchar(70) NOT NULL,
+  `tanggal_lahir_bagian` date NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp_bagian` varchar(12) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_suratmasuk`
+--
+
+CREATE TABLE `tb_suratmasuk` (
+  `id_suratmasuk` int(11) NOT NULL,
+  `tanggalmasuk_suratmasuk` datetime NOT NULL DEFAULT current_timestamp(),
+  `kategori` varchar(20) NOT NULL,
+  `nomor_suratmasuk` varchar(25) NOT NULL,
+  `tanggalsurat_suratmasuk` date NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `file_suratmasuk` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_suratmasuk`
+--
+
+INSERT INTO `tb_suratmasuk` (`id_suratmasuk`, `tanggalmasuk_suratmasuk`, `kategori`, `nomor_suratmasuk`, `tanggalsurat_suratmasuk`, `judul`, `file_suratmasuk`) VALUES
+(1, '2021-10-25 20:42:41', 'Pengumuman', '2021/01/20', '2021-10-01', 'Libur Semester', ''),
+(2, '2021-10-25 20:43:16', 'Pengumuman', '2021/01/20', '2021-10-08', 'Pembelajaran Luring', ''),
+(3, '2021-10-25 20:43:48', 'Surat', '2021/01/21', '2021-10-01', 'Permohonan Izin', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username_admin` (`username_admin`);
+
+--
+-- Indexes for table `tb_bagian`
+--
+ALTER TABLE `tb_bagian`
+  ADD PRIMARY KEY (`id_bagian`),
+  ADD UNIQUE KEY `username_admin_bagian` (`username_admin_bagian`);
+
+--
+-- Indexes for table `tb_suratmasuk`
+--
+ALTER TABLE `tb_suratmasuk`
+  ADD PRIMARY KEY (`id_suratmasuk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1931710010;
+
+--
+-- AUTO_INCREMENT for table `tb_bagian`
+--
+ALTER TABLE `tb_bagian`
+  MODIFY `id_bagian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `tb_suratmasuk`
+--
+ALTER TABLE `tb_suratmasuk`
+  MODIFY `id_suratmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
